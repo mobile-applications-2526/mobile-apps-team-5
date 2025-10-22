7adc46b816faa651992fb37f3e2308957b84a793
Iniazst <iniaz.steyaert@gmail.com>
2025-10-22 14:30:57 +0200

    added first basic project structure

---
+++ team-5-mobile-apps/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'team-5-mobile-apps',
+  webDir: 'www'
+};
+
+export default config;
